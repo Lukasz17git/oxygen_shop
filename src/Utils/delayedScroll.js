ea060e87057c07066f2e1421e51811b43e9bb1bf
Lukasz17git <lucas.m2295@gmail.com>
2023-05-16 22:16:15 +0200

    changed location of to-the-top button

--- src/Utils/delayedScroll.js
+++ src/Utils/delayedScroll.js
@@ -1,11 +1,12 @@
 const toTopID = "to-top"
 
 export const addDelayedScroll = (elementID = toTopID) => {
-    document.getElementById(elementID).addEventListener('click', (e) => {
-        e.preventDefault()
-        setTimeout(() => {
-            window.scrollTo({ top: 0, left: 0 })
-            window.history.pushState('', '', '/')
-        }, 200)
-    })
+   document.getElementById(elementID).addEventListener('click', (e) => {
+      e.preventDefault()
+      setTimeout(() => {
+         window.scrollTo({ top: 0, left: 0 })
+         console.log(window.location.pathname)
+         window.history.pushState('', '', window.location.pathname)
+      }, 200)
+   })
 }
